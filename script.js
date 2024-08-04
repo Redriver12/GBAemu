d57c228e204d2d785fb9d22fd0bc68779b8e1090
Redriver12 <nodiscotd@gmail.com>
2024-08-04 10:10:54 +0000

    Update script.js

--- script.js
+++ script.js
@@ -9,21 +9,6 @@ async function loadRom(event) {
     const file = event.target.files[0];
     if (file) {
         const arrayBuffer = await file.arrayBuffer();
-        <script src="https://cdn.jsdelivr.net/gh/endrift/gbajs/gbajs.js"></script>
-<script>
-   async function loadRom(event) {
-       const file = event.target.files[0];
-       if (file) {
-           const arrayBuffer = await file.arrayBuffer();
-           const uint8Array = new Uint8Array(arrayBuffer);
-
-           const gba = new GameBoyAdvance();
-           gba.setCanvas(document.getElementById('emulatorCanvas'));
-           gba.loadRomFromBuffer(uint8Array);
-           gba.runStable();
-       }
-   }
-</script>
-
+        // Xử lý việc tải và chạy ROM tại đây
     }
 }
